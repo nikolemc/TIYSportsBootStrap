62ffa62e5efc0a8dbafce43527c1b621ef7de4fc
nikolemc <nikolemcstanley@gmail.com>
2017-04-19 10:33:31 -0400

    added facebook logo and panel

--- TIYSports/App_Start/BundleConfig.cs
+++ TIYSports/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TIYSports
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css", "~/Content/font-awesome.min.css",
                       "~/Content/site.css"));
         }
     }
